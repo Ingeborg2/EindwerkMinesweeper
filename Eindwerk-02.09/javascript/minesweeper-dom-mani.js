087ed89bba3b3057310d694098e5c604b7bf4b5e
Kristof De Laet <stoffel@MacBook-Pro-van-Kristof.local>
2017-09-08 16:18:53 +0200

    nieuw startpunt

---
+++ Eindwerk-02.09/javascript/minesweeper-dom-mani.js
@@ -0,0 +1,13 @@
+// 'use strict';
+
+// $('#topspeler').hide()
+
+// $('#btn').click(function () {
+//     $('#btn_startGame').addClass('verschijn2')
+
+//     ('#topspeler').addClass('tableopacity')
+//     $('#hidebutton').removeClass('buttonhide')
+//     $('#topspeler').show()
+//     if ($('.listitem')) {
+//         $('.listitem').remove()
+//     }})
