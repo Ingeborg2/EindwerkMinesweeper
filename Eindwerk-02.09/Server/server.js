86b23769e77e2d686f49e5ad012f02b1ab427022
Maxime Van Eijndhoven <maxime-vaneijndhoven@hotmail.com>
2017-09-13 12:17:38 +0200

    server in orde

--- Eindwerk-02.09/Server/server.js
+++ Eindwerk-02.09/Server/server.js
@@ -37,17 +37,27 @@ app.get("/spelers", function (request, response) {
         console.log("Connected successfully to server");
         console.log(qryObj)
         var collection = db.collection('spelers');
-        collection.find(qryObj, { name: 1, rows: 1, cols: 1, time: 1, mines: 1 , date: 1}).sort({ time: 1, date: 1 }).toArray(function (err, docs) {
+            collection.aggregate([{$match: qryObj},{$sort : {time:1,date:1}},{$group : {_id : {rows: '$rows' ,cols: '$cols',mines: '$mines'}, score: {$push : { name : '$name',time: '$time', date : '$date'}}}}]).toArray(function (err, docs) {
             console.log("Speler document(s) found:");
-            response.end(JSON.stringify(docs));
+            var result = []
+            for (var i = 0; i<docs.length;i++){
+                result.push({
+                    rows : docs[i]._id.rows,
+                    cols : docs[i]._id.cols,
+                    mines : docs[i]._id.mines,
+                    score : docs[i].score.slice(0,3)
+                })
+            }
+            response.end(JSON.stringify(result));
             db.close();
         });
-    })
+
+        })
+       
 })
 
 app.post("/spelers", function (request, response) {
-    var date = new Date()
-
+    var date = formatDate(new Date())
     console.log(request.body)
     var event = {
         name: request.body.name,
@@ -64,6 +74,7 @@ app.post("/spelers", function (request, response) {
         collection.insertOne(event, function (err, r) {
             if (!err) {
                 console.log("Gelukt")
+                console.log(date)
                 response.end(JSON.stringify({ message: "gelukt", status: "200" }))
             }
             else {
@@ -81,3 +92,12 @@ var server = app.listen(8081, function () {
     var port = server.address().port;
 });
 
+function formatDate(d){
+    var year = d.getFullYear();
+    var month = d.getMonth();
+    var day = d.getDay();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+    var seconds = d.getSeconds();
+    return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds
+  }
--- Eindwerk-02.09/javascript/minesweepercontroller.js
+++ Eindwerk-02.09/javascript/minesweepercontroller.js
@@ -249,8 +249,9 @@ function clickfunctie() {
             crossDomain: true,
             success: function (json) {
                 $.each(json, function (k, v) {
-                    $("#topspeler").append($('<tr>').attr('class', 'listitem').html("<td>" + v.name + "</td> <td>" + v.rows + "</td><td>" + v.cols + "</td><td>" + v.time + "</td><td>" + v.mines + "</td><td>"+ v.date+"</td>"))
-                });
+                    for(var i = 0; i<v.score.length;i++){
+                    $("#topspeler").append($('<tr>').attr('class', 'listitem').html("<td>" + v.score[i].name + "</td> <td>" + v.rows + "</td><td>" + v.cols + "</td><td>" + v.score[i].time + "</td><td>" + v.mines + "</td><td>"+ v.score[i].date+"</td>"))
+                }});
             },
 
             error: function(XMLHttpRequest, textStatus, errorThrown) {
